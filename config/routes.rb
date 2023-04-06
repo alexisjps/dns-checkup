ae9938f4131d2d6febb72c88ec2299f7609f2e82
Alexis Stephan <alexis-stephan@hotmail.fr>
2023-04-06 08:59:06 +0200

    done
    Signed-off-by: Alexis Stephan <alexis-stephan@hotmail.fr>

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root to: "dns_lookups#lookup"
+  get 'dns_lookups/lookup', to: 'dns_lookups#lookup', as: 'dns_lookups_lookup'
 end
