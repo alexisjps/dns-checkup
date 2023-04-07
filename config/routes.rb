434cbd19913f3e1a93d835e9c4244d38a208c2e8
Alexis Stephan <alexis-stephan@hotmail.fr>
2023-04-07 11:30:08 +0200

    restart the project
    Signed-off-by: Alexis Stephan <alexis-stephan@hotmail.fr>

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,2 @@
 Rails.application.routes.draw do
-  root to: "dns_lookups#lookup"
-  get 'dns_lookups/lookup', to: 'dns_lookups#lookup', as: 'dns_lookups_lookup'
 end
--- test/controllers/dns_lookups_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class DnsLookupsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
